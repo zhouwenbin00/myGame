e25465b6090af68e185f0e1d590bfb793ce36747
zhouwenbin <2825075112@qq.com>
2019-07-17 21:14:51 +0800

    1

---
+++ controller/src/main/java/Application.java
@@ -0,0 +1,24 @@
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
+import org.apache.commons.lang3.Validate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/** @Auther: zhouwenbin @Date: 2019/7/17 20:48 */
+public class Application {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        try {
+            // 字符集判断
+            Validate.isTrue(
+                    StringUtils.equalsIgnoreCase(SystemUtils.FILE_ENCODING, "utf-8"),
+                    "字符编码必须是utf-8");
+
+        } catch (Exception e) {
+            LOGGER.error("启动失败!", e);
+            System.exit(1);
+        }
+    }
+}
